44655c0768ac5d953cb0f80d628e2b221b9f9af5
deborars <deboraro.61@gmail.com>
2024-12-08 13:00:03 -0300

    Projeto em angular para postar momentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/page/home/home.component';
+import { AboutComponent } from './components/page/about/about.component';
+import { NewMomentComponent } from './components/page/new-moment/new-moment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    NewMomentComponent
   ],
   imports: [
     BrowserModule,
