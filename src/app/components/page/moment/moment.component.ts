6ab4038efa7caa3864dddf838f9cca17fa151002
deborars <deboraro.61@gmail.com>
2024-12-30 11:39:14 -0300

    CSS página de Momentos - #36

--- src/app/components/page/moment/moment.component.ts
+++ src/app/components/page/moment/moment.component.ts
@@ -1,9 +1,9 @@
 import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-
 import { MomentService } from '../../../services/moment.service';
-
 import { Moments } from '../../../Moments';
+import { environment } from '../../../../environments/environment';
+import { faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-moment',
@@ -12,6 +12,10 @@ import { Moments } from '../../../Moments';
 })
 export class MomentComponent {
   moment?: Moments;
+  baseApiUrl = environment.baseApiUrl;
+
+  faTimes = faTimes;
+  faEdit = faEdit;
 
   constructor(private momentService: MomentService, private route: ActivatedRoute){}
 
